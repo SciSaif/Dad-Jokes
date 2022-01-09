aae7e434c918af6378f1b047219d62942aa2f3e5
scisaif <saiflll284@gmail.com>
2022-01-10 02:50:29 +0530

    added joke arena

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const joke = document.querySelector('#joke');
 const btn = document.querySelector('#btn');
 const loader = document.querySelector('.loader');
-const switchPage = document.querySelector('.switch-Page');
-const headerMain1 = document.querySelector('.header-main');
-const headerMain2 = document.querySelector('.header-main2');
+const dadJokes = document.querySelector('.dad-jokes');
+const chuckNorris = document.querySelector('.chuck-norris');
+const jokeArena = document.querySelector('.joke-arena');
+const root = document.documentElement;
+const secondPage = document.querySelector('.second-page');
+const thirdPage = document.querySelector('.third-page');
+const typeSelect = document.querySelector('#type');
+const typeSelectBox = document.querySelector('.type-select-box');
 
-let page = "dad jokes";
+let page = "Joke Arena";
+let type = typeSelect.value;
 
+const colors = [
+    {"primary" :"#2a9d8f",
+     "dark-primary": "#264653",
+     "tertiary" : "#e9c46a", 
+     "secondary" : "#f4a261",
+     "dark-secondary" : "#e76f51"},
+
+     {"primary" :"#457b9d",
+     "dark-primary": "#1d3557",
+     "tertiary" : "#a8dadc", 
+     "secondary" : "#f1faee",
+     "dark-secondary" : "#e63946"}];
+
+const switchTheme  = (theme) => {
+    root.style.setProperty('--primary', colors[theme].primary);
+    root.style.setProperty('--dark-primary', colors[theme]["dark-primary"]);
+    root.style.setProperty('--tertiary', colors[theme].tertiary);
+    root.style.setProperty('--secondary', colors[theme].secondary);
+    root.style.setProperty('--dark-secondary', colors[theme]["dark-secondary"]);
+}
 
 const getChuckJoke = async () => {
     try{
@@ -29,16 +55,26 @@ const getDadJoke = async () => {
         return "Sorry! We are having trouble fetching jokes for ya :(";
     }
 }
-    
+
+const getRandomJoke = async () => {
+    try{
+        const res = await axios.get(`https://v2.jokeapi.dev/joke/${type}?type=single`);
+        return res.data.joke
+    } catch(e) {
+        return "Sorry! We are having trouble fetching jokes for ya :(";
+    }
+}
+
 
 const displayJoke = async () => {
     loader.classList.add('loader-active');
     let jokeText = "d";
-    if(page == "dad jokes"){
+    if(page == "Joke Arena"){
+        jokeText = await getRandomJoke();
+    }else if(page == "Dad Jokes"){
         jokeText = await getDadJoke();
     }else {
         jokeText = await getChuckJoke();
-        console.log(jokeText);
     }
     joke.innerHTML = jokeText;
     loader.classList.remove('loader-active');
@@ -47,27 +83,76 @@ const displayJoke = async () => {
 
 
 
+
+
   
 
 displayJoke();
-// getJoke2();
 
 btn.addEventListener('click', ()=> {
     displayJoke();
 })
 
+typeSelect.addEventListener('change', () => {
+    type = typeSelect.value;
+})
 
-switchPage.addEventListener('click', ()=> {
-    if(page == "dad jokes") {
-        page = "chuck jokes";
-        headerMain1.classList.add('hidden');
-        headerMain2.classList.remove('hidden');
-        switchPage.innerHTML = "Dad Jokes";
+
+secondPage.addEventListener('click', ()=> {
+    if(secondPage.innerHTML == "Dad Jokes") {
+        dadJokes.classList.remove('hidden');
+        chuckNorris.classList.add('hidden');
+        jokeArena.classList.add('hidden');
+        typeSelectBox.classList.add('hidden');
+        secondPage.innerHTML = page;
+        page = "Dad Jokes";
+        switchTheme(1);
+    }else if(secondPage.innerHTML == "Joke Arena"){
+        dadJokes.classList.add('hidden');
+        chuckNorris.classList.add('hidden');
+        jokeArena.classList.remove('hidden');
+        typeSelectBox.classList.remove('hidden');
+        secondPage.innerHTML = page;
+        page = "Joke Arena";
+        switchTheme(0);
     }else {
-        page = "dad jokes";
-        headerMain2.classList.add('hidden');
-        headerMain1.classList.remove('hidden');
-        switchPage.innerHTML = "Chuck Norris Jokes";
+        dadJokes.classList.add('hidden');
+        chuckNorris.classList.remove('hidden');
+        jokeArena.classList.add('hidden');
+        typeSelectBox.classList.add('hidden');
+        secondPage.innerHTML = page;
+        page = "Chuck Norris Jokes";
+        switchTheme(0);
+    }
+    displayJoke();
+    
+});
+
+thirdPage.addEventListener('click', ()=> {
+    if(thirdPage.innerHTML == "Dad Jokes") {
+        dadJokes.classList.remove('hidden');
+        chuckNorris.classList.add('hidden');
+        jokeArena.classList.add('hidden');
+        typeSelectBox.classList.add('hidden');
+        thirdPage.innerHTML = page;
+        page = "Dad Jokes";
+        switchTheme(1);
+    }else if(thirdPage.innerHTML == "Joke Arena"){
+        dadJokes.classList.add('hidden');
+        chuckNorris.classList.add('hidden');
+        jokeArena.classList.remove('hidden');
+        typeSelectBox.classList.remove('hidden');
+        thirdPage.innerHTML = page;
+        page = "Joke Arena";
+        switchTheme(0);
+    }else {
+        dadJokes.classList.add('hidden');
+        chuckNorris.classList.remove('hidden');
+        jokeArena.classList.add('hidden');
+        typeSelectBox.classList.add('hidden');
+        thirdPage.innerHTML = page;
+        page = "Chuck Norris Jokes";
+        switchTheme(0);
     }
     displayJoke();
     
