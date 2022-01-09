33d7a7ff5483cfb88e914b19c85d970fd38b366e
scisaif <saiflll284@gmail.com>
2022-01-09 22:32:48 +0530

    added loader

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const joke = document.querySelector('#joke');
 const btn = document.querySelector('#btn');
+const loader = document.querySelector('.loader');
 
 
 const displayJoke = async () => {
+    loader.classList.add('loader-active');
     const jokeText = await getJoke();
     console.log(jokeText);
     joke.innerHTML = jokeText;
+    loader.classList.remove('loader-active');
 }
 
 const getJoke = async () => {
     try{
-
         const config = {headers: {Accept: 'application/json'}};
         const res = await axios.get('https://icanhazdadjoke.com/', config);
         return res.data.joke;
