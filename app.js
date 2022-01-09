129fdb14701536812ae339c984e7e607b3852da9
scisaif <saiflll284@gmail.com>
2022-01-10 00:15:24 +0530

    Added chuck norris jokes

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const joke = document.querySelector('#joke');
 const btn = document.querySelector('#btn');
 const loader = document.querySelector('.loader');
+const switchPage = document.querySelector('.switch-Page');
+const headerMain1 = document.querySelector('.header-main');
+const headerMain2 = document.querySelector('.header-main2');
+
+let page = "dad jokes";
 
 
-const displayJoke = async () => {
-    loader.classList.add('loader-active');
-    const jokeText = await getJoke();
-    console.log(jokeText);
-    joke.innerHTML = jokeText;
-    loader.classList.remove('loader-active');
+const getChuckJoke = async () => {
+    try{
+        const config = {headers: {accept: 'application/json',
+        'x-rapidapi-host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
+        'x-rapidapi-key': 'cd8fccb11cmshce49708fdfb89aap1aaf3bjsn4dce7cf0af34'}};
+        const res = await axios.get('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', config);
+        return res.data.value;
+    } catch(e) {
+        return "Sorry! We are having trouble fetching jokes for ya :(";
+    }
 }
 
-const getJoke = async () => {
+const getDadJoke = async () => {
     try{
         const config = {headers: {Accept: 'application/json'}};
         const res = await axios.get('https://icanhazdadjoke.com/', config);
@@ -20,9 +29,46 @@ const getJoke = async () => {
         return "Sorry! We are having trouble fetching jokes for ya :(";
     }
 }
+    
+
+const displayJoke = async () => {
+    loader.classList.add('loader-active');
+    let jokeText = "d";
+    if(page == "dad jokes"){
+        jokeText = await getDadJoke();
+    }else {
+        jokeText = await getChuckJoke();
+        console.log(jokeText);
+    }
+    joke.innerHTML = jokeText;
+    loader.classList.remove('loader-active');
+}
+
+
+
+
+  
 
 displayJoke();
+// getJoke2();
 
 btn.addEventListener('click', ()=> {
     displayJoke();
 })
+
+
+switchPage.addEventListener('click', ()=> {
+    if(page == "dad jokes") {
+        page = "chuck jokes";
+        headerMain1.classList.add('hidden');
+        headerMain2.classList.remove('hidden');
+        switchPage.innerHTML = "Dad Jokes";
+    }else {
+        page = "dad jokes";
+        headerMain2.classList.add('hidden');
+        headerMain1.classList.remove('hidden');
+        switchPage.innerHTML = "Chuck Norris Jokes";
+    }
+    displayJoke();
+    
+});
